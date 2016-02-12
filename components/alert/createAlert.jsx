93069689952715e58ddf0c7008df44876b097a11
Michael McGrath & tania <pair+michaelmcgrath.tania-b@devbootcamp.com>
2016-02-13 11:36:47 +1300

    Hour and minute count up and count down buttons wrap

--- components/alert/createAlert.jsx
+++ components/alert/createAlert.jsx
@@ -9,53 +9,75 @@ class CreateAlert extends Component {
   }
 
   upHourClick(e) {
-    this.setState({
-      hr: this.state.hr +1
-    })
+    if(this.state.hr ===24){
+      this.setState({
+        hr: this.state.hr -24
+      })
+    } else{
+      this.setState({
+        hr: this.state.hr +1
+      })
+    }
   }
 
   downHourClick(e) {
-    this.setState({
-      hr: this.state.hr -1
-    })
+    if(this.state.hr ===0){
+      this.setState({
+        hr: this.state.hr +24
+      })
+    } else{
+      this.setState({
+        hr: this.state.hr -1
+      })
+    }
   }
 
   upMinuteClick(e) {
-    this.setState({
-      min: this.state.min +1
-    })
+    if(this.state.min ===59){
+      this.setState({
+        min: this.state.min -59
+      })
+    } else{
+      this.setState({
+        min: this.state.min +1
+      })
+    }
   }
 
   downMinuteClick(e) {
-    this.setState({
-      min: this.state.min -1
-    })
+    if(this.state.min ===0){
+      this.setState({
+        min: this.state.min +59
+      })
+    } else{
+      this.setState({
+        min: this.state.min -1
+      })
+    }
   }
-
-
 
   render() {
     return(
       <div>
-        <Link to={`/home`}>Home</Link>
-        <div><Link to={`/alert`}>Back</Link></div>
-          <div className="hours">
-            <button onClick={this.upHourClick.bind(this)}>Count Up!</button>
-            <h1>{this.state.hr}</h1>
-            <button onClick={this.downHourClick.bind(this)}>Count Down!</button>
-          </div>
-          <div className="minutes">
-            <button onClick={this.upMinuteClick.bind(this)}>Count Up!</button>
-            <h1>{this.state.min}</h1>
-            <button onClick={this.downMinuteClick.bind(this)}>Count Down!</button>
-          </div>
-          <div>
-            <button>Save</button>
-            <button>Delete</button>
-            <button>Cancel</button>
-          </div>
+      <Link to={`/home`}>Home</Link>
+      <div><Link to={`/alert`}>Back</Link></div>
+      <div className="hours">
+      <button onClick={this.upHourClick.bind(this)}>Count Up!</button>
+      <h1>{this.state.hr}</h1>
+      <button onClick={this.downHourClick.bind(this)}>Count Down!</button>
       </div>
-    )
+      <div className="minutes">
+      <button onClick={this.upMinuteClick.bind(this)}>Count Up!</button>
+      <h1>{this.state.min}</h1>
+      <button onClick={this.downMinuteClick.bind(this)}>Count Down!</button>
+      </div>
+      <div>
+      <button>Save</button>
+      <button>Delete</button>
+      <button>Cancel</button>
+      </div>
+      </div>
+      )
   }
 }
 
