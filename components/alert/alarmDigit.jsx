dd53e03a5ad7c669ff9fcc9f6a5ee0e256aad8df
Michael McGrath & tania <pair+michaelmcgrath.tania-b@devbootcamp.com>
2016-02-13 20:55:52 +1300

    React that code

--- components/alert/alarmDigit.jsx
+++ components/alert/alarmDigit.jsx
@@ -3,33 +3,12 @@ import React, {Component} from 'react'
 class AlarmDigit extends Component {
   constructor(props){
     super()
-    this.state = {
-      value: 0
-    }
-  }
-
-  handleIncrease() {
-    this.state.value ++
-    if (this.state.value >= this.props.numberSystem) {
-      this.state.value = 0
-    }
-    this.setState(this.state)
-  }
-
-  handleDecrease() {
-    this.state.value --
-    if (this.state.value < 0) {
-      this.state.value = this.props.numberSystem
-    }
-    this.setState(this.state)
   }
 
   render() {
     return(
       <div>
-        <button onClick={this.handleIncrease.bind(this)}>++</button>
-        <p>{this.state.value}</p>
-        <button onClick={this.handleDecrease.bind(this)}>--</button>
+        <p>{this.props.counterVal}</p>
       </div>
       )
     }
--- components/alert/createAlert.jsx
+++ components/alert/createAlert.jsx
@@ -5,6 +5,43 @@ import AlarmDigit from './alarmDigit.jsx'
 class CreateAlert extends Component {
   constructor(props){
     super(props)
+
+    this.state = {
+      counter1: 1,
+      counter2: 2
+    }
+  }
+
+  handleHrIncrease() {
+    this.state.counter1 ++
+    // if (this.state.counter1 >= this.props.numberSystem) {
+    //   this.state.counter1 = 0
+    // }
+    this.setState(this.state)
+  }
+
+  handleHrDecrease() {
+    this.state.counter1 --
+    // if (this.state.counter1 < 0) {
+    //   this.state.counter1 = this.props.numberSystem
+    // }
+    this.setState(this.state)
+  }
+
+  handleIncrease() {
+    this.state.counter2 ++
+    // if (this.state.counter1 >= this.props.numberSystem) {
+    //   this.state.counter1 = 0
+    // }
+    this.setState(this.state)
+  }
+
+  handleDecrease() {
+    this.state.counter2 --
+    // if (this.state.counter1 < 0) {
+    //   this.state.counter1 = this.props.numberSystem
+    // }
+    this.setState(this.state)
   }
 
   render() {
@@ -13,13 +50,25 @@ class CreateAlert extends Component {
       <Link to={`/home`}>Home</Link>
       <div><Link to={`/alert`}>Back</Link></div>
 
-        <div className="hours">
-          <AlarmDigit numberSystem={24} ref="hourDigit" />
+        <div id="hours" className="hours">
+          <button onClick={this.handleHrIncrease.bind(this)}>++</button>
+          <AlarmDigit numberSystem={24} counterVal={this.state.counter1} ref="hourDigit" />
+          <button onClick={this.handleHrDecrease.bind(this)}>--</button>
         </div>
 
         <div className="minutes">
-          <AlarmDigit numberSystem={60} ref="minuteDigit" />
+          <button onClick={this.handleIncrease.bind(this)}>++</button>
+          <AlarmDigit numberSystem={60} counterVal={this.state.counter2} ref="minuteDigit" />
+          <button onClick={this.handleDecrease.bind(this)}>--</button>
         </div>
+
+        <div className="buttons">
+          <button>Save</button>
+          <button>Cancel</button>
+          <button>Delete</button>
+        </div>
+
+
 
       </div>
       )
