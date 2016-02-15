96cca9376f43c053c48645df4ed7a85668f17f9b
Colin Boyd <colinboyd@Colins-MacBook-Air.local>
2016-02-16 11:32:53 +1300

    extracting json data into time data and it's working :banana:

--- components/graph/graph.jsx
+++ components/graph/graph.jsx
@@ -2,11 +2,23 @@ import React, {Component} from 'react'
 import {Link} from 'react-router'
 import Highcharts from 'highcharts'
 import ReactHighcharts from 'react-highcharts/bundle/highcharts'
+import {users} from '../../test-data.json'
 
 // var sevenDayChart = filter through by most recent 7 days and put values into data array
 // unix week is 604800 seconds
 
 
+
+const getChartTimeData = (data) => {
+  var chartTimeData = []
+  for (var i=0; i<(users[0].tests).length; i++ ) {
+    chartTimeData.push(users[0].tests[i].timestamp)
+  }
+  return chartTimeData
+}
+
+
+console.log(getChartTimeData())
 
 var config = {
 chart: {
@@ -15,39 +27,27 @@ chart: {
           borderRadius: 6,
           type: 'line'
       },
-  title: {
-    text: 'Weekly Blood Sugar Levels'
-  },
+  title: { text: 'Weekly Blood Sugar Levels'},
   xAxis: {
   //categories: ['1455483473654', '1455483373654', '1455483373654','Feb 2', 'Feb 3', 'Feb 4', 'Feb 5', 'Feb 6', 'Feb 7'],
     type: 'datetime',
     dateTimeLabelFormats: {
               hour: '%l:%M %p'
           },
-           labels: {
-                  formatter: function() {
-                      return Highcharts.dateFormat('%a %d %b', this.value) }}
+           labels: { formatter: function() { return Highcharts.dateFormat('%a %d %b', this.value) }}
   },
   yAxis: {
     title: {
-              enabled: true,
-              text: 'BSL',
-              style: {
-                  fontWeight: 'normal'
+            enabled: true,
+            text: 'mmol/L',
+            style: { fontWeight: 'normal' }
             }
-          }
   },
-  legend: {
-            enabled: false
-        },
-  credits: {
-            enabled: true
-        },
+  legend: { enabled: false },
   series: [{
     name: 'BSL',
     data: [12, 10.5, 10.4, 12.2, 14, 17, 13.6, 14.5, 21.4, 19.1, 9.6, 5.4]
   }],
-
 }
 
 
@@ -58,7 +58,6 @@ class BloodTestChart extends Component {
        chart.series[0].addPoint({x: 10, y:12})
      }
 
-
     render() {
       return (
       <div>
@@ -67,4 +66,5 @@ class BloodTestChart extends Component {
       </div>)
     }
 }
+
 export default BloodTestChart
