e9010e8e2ff6740fa2de4a9ccae41e424b692793
Piet Geursen <pietgeursen@gmail.com>
2016-02-16 10:25:55 +1300

    Very very incomplete

--- reducer.js
+++ reducer.js
@@ -1,16 +1,45 @@
 import {fromJS, Map} from 'immutable'
+import Firebase from 'firebase'
+
 import data from './test-data.json'
 
 const INITIAL_STATE = fromJS(data.users[0])
 
-// console.log(INITIAL_STATE.get('alerts').first().get('time'))
+function authDataCallback(authData) {
+  if (authData) {
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+  } else {
+    console.log("User is logged out");
+  }
+}
+var ref = new Firebase("https://diabetezy-server.firebaseio.com");
+
+ref.onAuth(authDataCallback);
+
+function doFirebaseLogin() {
+  
+  ref.authWithOAuthPopup("github", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+}
+
+import $ from 'jquery'
+
+$('a').click(function(e) {
+ e.preventDefault() 
+ doFirebaseLogin() 
+})
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
-    case 'CREATE_ALERT':
-      var alertMap = Map({id: 2, time: action.time})
+  case 'CREATE_ALERT':
+    var alertMap = Map({id: 2, time: action.time})
       return state.set('alerts', state.get('alerts').push(alertMap))
-    default:
+  default:
       return state
   }
 } 
