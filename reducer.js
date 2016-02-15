246439bfe2711ae7dceb3c13203c868dbc5d726b
Tania-B <Tania-B@users.noreply.github.com>
2016-02-15 18:03:57 +1300

    able to create new unique ids for alerts

--- reducer.js
+++ reducer.js
@@ -5,15 +5,19 @@ const INITIAL_STATE = fromJS(data.users[0])
 
 // console.log(INITIAL_STATE.get('alerts').first().get('time'))
 
+let nextAlertId = 12091985
+
 const reducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'CREATE_ALERT':
-      var alertMap = Map({id: 2, time: action.time})
+      nextAlertId++
+      const alertMap = Map({id: nextAlertId, time: action.time})
       return state.set('alerts', state.get('alerts').push(alertMap))
     default:
       return state
   }
-} 
+}
+
 export default reducer
 
 // const newState = reducer(undefined, {type: "CREATE_ALERT", time: "12:30"})
