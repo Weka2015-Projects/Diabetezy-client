373ab9106de4f3f2cc187c2678d7f0f44bf413c3
Colin Boyd <colinboyd@Colins-MacBook-Air.local>
2016-02-15 18:32:41 +1300

    Finally got HighCharts rendering on graph page :pineapple:

--- components/graph/graph.jsx
+++ components/graph/graph.jsx
@@ -1,40 +1,30 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
-import Chart from 'c3-react'
-console.log(Chart)
-import {Line as LineChart} from 'react-chart.js';
+import Highcharts from 'highcharts'
+import ReactHighcharts from 'react-highcharts/bundle/highcharts'
 
-class DashboardComponent extends Component {
-  render() {
-  var chart = c3.generate({
-         bindto: '#chart1',
-         data: {
-           json: data,
-           keys: {
-               x: 'date',
-               value: ['requests', 'revenue']
-           },
-           type: 'spline',
-           types: { 'revenue': 'bar' },
-           axes: {
-             'requests': 'y',
-             'revenue': 'y2'
-           }
-         },
-         axis: {
-             x: { type: 'timeseries' },
-             y2: { show: true }
-         }
-     })
+var config = {
+  title: {
+    text: 'Weekly Blood Sugar Levels'
+  },
+  xAxis: {
+    categories: ['Feb 1', 'Feb 1', 'Feb 1','Feb 2', 'Feb 3', 'Feb 4', 'Feb 5', 'Feb 6', 'Feb 7']
+  },
+  series: [{
+    data: [12, 10.5, 10.4, 12.2, 14, 17, 13.6, 14.5, 21.4, 19.1, 9.6, 5.4]
+  }],
 
-
-    return (
-      <div className="dashboard-component container">
-        <div id="testchart"></div>
-        <Chart data={chartData} element='testchart' type='Line' />
-      </div>
-    )
-  }
 }
 
-export default DashboardComponent
+class BloodTestChart extends Component {
+    componentDidMount() {
+       let chart = this.refs.chart.getChart()
+       chart.series[0].addPoint({x: 10, y:12})
+     }
+
+    render() {
+      return <ReactHighcharts config={config} ref="chart"></ReactHighcharts>
+    }
+}
+
+export default BloodTestChart
