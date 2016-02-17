2eec2a3a6c9e973af41026665949d78478ef3848
Tania-B <Tania-B@users.noreply.github.com>
2016-02-17 15:06:11 +1300

    everythinggg is not awesome....

--- components/alert/editAlert.jsx
+++ components/alert/editAlert.jsx
@@ -5,19 +5,18 @@ import {connect} from 'react-redux'
 import {Button} from 'react-bootstrap'
 import {saveAlert} from '../../firebaseWrapper'
 
-class CreateAlert extends Component {
+class EditAlert extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
       hourCounter: 0,
       minuteCounter: 0,
       hourCounterLimit: 24,
-      minuteCounterLimit: 59
+      minuteCounterLimit: 59,
     }
   }
 
-  // counter limits
+  // handle counter Limits
   handleCounterLimits = () => {
     if (this.state.hourCounter > this.state.hourCounterLimit) { this.state.hourCounter = 0 }
     if (this.state.hourCounter < 0) { this.state.hourCounter = this.state.hourCounterLimit }
@@ -51,31 +50,6 @@ class CreateAlert extends Component {
     this.setState(this.state)    
   };
 
-  // handle time conversion: counter to DB
-  handleCountertoDB = () => {
-    let hour = this.state.hourCounter
-    let minute = this.state.minuteCounter
-    hour = (hour < 10) ? '0' + hour.toString() : hour.toString()
-    minute = (minute < 10) ? '0' + minute.toString() : minute.toString()
-    let time = moment(hour + minute, 'hmm').format('HH:mm')
-    return time
-  };
-
-  // button actions
-  saveButton = () => {
-    let time = this.handleCountertoDB()
-    this.props.saveNewAlert(time)
-  };
-
-  cancelButton = () => {
-    console.log('cancel')
-  };
-
-  deleteButton = (time) => {
-    console.log("delete")
-    //delete alert from firebase
-  };
-
   render() {
     return(
       <div>
@@ -83,41 +57,46 @@ class CreateAlert extends Component {
           <Link to={`/home`}>Home</Link><br />
           <Link to={`/alert`}>Back</Link>
         </div>
-        
-        <h2>CREATE</h2>
-        
-        <div className="hours">
-          <button onClick={this.handleHourIncrease}>++</button>
-            <h2>{this.state.hourCounter}</h2>
-          <button onClick={this.handleHourDecrease}>--</button>        
-        </div>
 
-        <div className="minutes">        
-          <button onClick={this.handleMinuteIncrease}>++</button>        
-            <h2>{this.state.minuteCounter}</h2>
-          <button onClick={this.handleMinuteDecrease}>--</button>        
-        </div>
+      <h2>EDIT</h2>
+      
+      <div className="hours">
+        <button onClick={this.handleHourIncrease}>++</button>
+          <h2>{this.state.hourCounter}</h2>
+        <button onClick={this.handleHourDecrease}>--</button>        
+      </div>
 
-        <div className="buttons">
-            <button onClick={this.saveButton}>Save Time</button>
-            <button onClick={this.cancelButton}>Cancel</button>
-            <button onClick={this.deleteButton}>Delete Time</button>
-          </div>        
+      <div className="minutes">        
+        <button onClick={this.handleMinuteIncrease}>++</button>        
+          <h2>{this.state.minuteCounter}</h2>
+        <button onClick={this.handleMinuteDecrease}>--</button>        
+      </div>
 
-      </div>           
-      )
-    }
+      <div className="buttons">
+          <button onClick={this.saveButton}>Save Time</button>
+          <button onClick={this.cancelButton}>Cancel</button>
+          <button onClick={this.deleteButton}>Delete Time</button>
+      </div>
+    </div>
+    )
   }
+}
 
   const mapDispatchToProps = (dispatch) => {
     return {
       saveNewAlert: (time) => {
         saveAlert({time: time}, (id) => {
-          console.log(id)
           dispatch({type: "CREATE_ALERT", id: id, time: time})
         })
       }
     }
   }
 
-export default connect(undefined, mapDispatchToProps)(CreateAlert)
+export default connect(undefined, mapDispatchToProps)(EditAlert)
+
+// // if alert already exists do this:
+// if (this.state.alertTime || this.state.id != undefined) {
+//   let alertTime = this.props.params.time
+//   let splitAlertHour = (this.state.hourCounter = parseInt(alertTime.substring(0,2)))
+//   let splitAlertMinute = (this.state.minuteCounter = parseInt(alertTime.substring(3,5)))
+// }
--- reducer.js
+++ reducer.js
@@ -4,12 +4,13 @@ const INITIAL_STATE = Map({ tests: [], alerts: [] })
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
+
     case 'CREATE_ALERT':
-      const alertMap = Map({id: action.id, time: action.time})
-      return state.set('alerts', state.get('alerts').set(action.id, alertMap))
+      const addAlert = Map({id: action.id, time: action.time})
+      return state.set('alerts', state.get('alerts').set(action.id, addAlert))
 
     case 'OVERWRITE_STATE':
-      return action.state
+      return action.state      
 
     case 'CREATE_BLOOD_TEST':
       const addBloodTest = Map({id:action.id, timestamp: action.timestamp, value: action.value})
