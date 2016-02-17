3581f8ef7b3e560f6bf2a07b9b238b1932c24ae7
katie.wrght@gmail.com <Katie.wrght@gmail.com>
2016-02-17 16:58:58 +1300

    Updates

---
+++ reducer.js
@@ -0,0 +1,27 @@
+import {fromJS, Map, List} from 'immutable'
+
+const INITIAL_STATE = Map({ tests: List.of(), alerts: List.of() })
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case 'CREATE_ALERT':
+      var alertMap = Map({id: 2, time: action.time})
+        return state.set('alerts', state.get('alerts').push(alertMap))
+
+    case 'OVERWRITE_STATE':
+      return fromJS(action.state)
+
+    case 'CREATE_BLOOD_TEST':
+      var addBloodTest = Map({id:action.id, timestamp: action.timestamp, value: action.value})
+      return state.set('tests', state.get('tests').set(action.id, addBloodTest))
+
+
+    case 'DELETE_BLOOD_TEST':
+      const deleteblood = state.deleteIn(['tests', action.id])
+      return deleteblood
+
+    default:
+      return state
+  }
+} 
+export default reducer
